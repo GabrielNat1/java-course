a56d3d4310c89178f970e82a2bcb9975c71fedf2
GabrielNat1 <dcbiel846@gmail.com>
2025-05-11 19:12:56 -0300

    "feat: added repository"

---
+++ src/academy/devdojo/maratona/introducao/TiposPrimitivos.java
@@ -0,0 +1,9 @@
+package academy.devdojo.maratona.introducao;
+
+public class TiposPrimitivos {
+    public static void main(String[] args) {
+        // int, double, float, char, byte, short, long, boolean
+        int age = 10;
+        System.out.println("The age this "+ age + "y");
+    }
+}
