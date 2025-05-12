1d7be0d38c7cece69141cff48e0712de1aad2c46
GabrielNat1 <dcbiel846@gmail.com>
2025-05-11 22:13:14 -0300

    feat: create class estruturaderepetiçao

---
+++ src/academy/devdojo/maratona/introducao/EstruturaDeRepetição.java
@@ -0,0 +1,15 @@
+package academy.devdojo.maratona.introducao;
+
+public class EstruturaDeRepetição {
+    public static void main(String[] args) {
+        // while, do while, for
+        int count = 0;
+        while(count < 10){
+            System.out.println(count++);
+        }
+
+        do {
+            System.out.println("teste");
+        } while(count < 10)
+    }
+}
