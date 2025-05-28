562480b92920d4ef046aae9352d3253ff51fc964
GabrielNat1 <dcbiel846@gmail.com>
2025-05-28 14:38:30 -0300

    feat: introducao metodos 02

--- src/academy/devdojo/maratona/javacore/Bintroducaometodos/dominio/Calculadora.java
+++ src/academy/devdojo/maratona/javacore/Bintroducaometodos/dominio/Calculadora.java
@@ -8,4 +8,8 @@ public class Calculadora {
     public void substrai(){
         System.out.println(20 - 1);
     }
+
+    public void multiplica(int num1, float num2){
+        System.out.println(num1 * num2);
+    }
 }
