2dd96c7bf1fa8875379a6117b67de1401077d23e
GabrielNat1 <dcbiel846@gmail.com>
2025-05-30 16:42:04 -0300

    feat: metodo Varargs 01

--- src/academy/devdojo/maratona/javacore/Bintroducaometodos/dominio/Calculadora.java
+++ src/academy/devdojo/maratona/javacore/Bintroducaometodos/dominio/Calculadora.java
@@ -44,4 +44,12 @@ public class Calculadora {
         System.out.println("num1: "+numero1);
         System.out.println("num2 "+numero2);
     }
+
+    public void somaArray(int[] numeros){
+        int soma = 0;
+        for(int num: numeros){
+            suma += num;
+            System.out.println(soma);
+        }
+    }
 }
