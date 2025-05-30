68e5def537bed7d1e42093c7f19c0a523c1377d8
GabrielNat1 <dcbiel846@gmail.com>
2025-05-30 17:10:45 -0300

    Exercise OO

---
+++ src/academy/devdojo/maratona/javacore/Bintroducaometodos/test/FuncionarioTest01.java
@@ -0,0 +1,15 @@
+package academy.devdojo.maratona.javacore.Bintroducaometodos.test;
+
+import academy.devdojo.maratona.javacore.Bintroducaometodos.dominio.Funcionario;
+
+public class FuncionarioTest01 {
+    public static void main(String[] args) {
+        Funcionario funcionario = new Funcionario();
+        funcionario.nome = "João";
+        funcionario.idade = 30;
+        funcionario.salario = new double[]{1500, 1700, 2000};
+
+        funcionario.imprimirDados();
+        funcionario.calcularMediaSalarios();
+    }
+}
