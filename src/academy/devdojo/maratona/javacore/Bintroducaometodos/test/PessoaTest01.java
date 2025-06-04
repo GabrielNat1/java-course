433a44e092e137b1660d924b9236ba0d2cc1cd64
GabrielNat1 <dcbiel846@gmail.com>
2025-06-04 16:48:56 -0300

    add test pessoa01

---
+++ src/academy/devdojo/maratona/javacore/Bintroducaometodos/test/PessoaTest01.java
@@ -0,0 +1,14 @@
+package academy.devdojo.maratona.javacore.Bintroducaometodos.test;
+
+import academy.devdojo.maratona.javacore.Bintroducaometodos.dominio.Pessoa;
+
+public class PessoaTest01 {
+    public static void main(String[] args) {
+        Pessoa pessoa = new Pessoa();
+        //pessoa.nome = "teste";
+        //pessoa.idade = 321;
+
+        pessoa.setNome("teste");
+
+    }
+}
