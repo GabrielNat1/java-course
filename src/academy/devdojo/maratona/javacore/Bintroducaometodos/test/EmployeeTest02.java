cc29e994812989401a7578dfa4b8c0284129fbdf
GabrielNat1 <dcbiel846@gmail.com>
2025-06-04 16:46:40 -0300

    feat: test employee 02

---
+++ src/academy/devdojo/maratona/javacore/Bintroducaometodos/test/EmployeeTest02.java
@@ -0,0 +1,16 @@
+/*package academy.devdojo.maratona.javacore.Bintroducaometodos.test;
+
+import academy.devdojo.maratona.javacore.Bintroducaometodos.dominio.Employee;
+
+public class EmployeeTest02 {
+    public static void main(String[] args) {
+        Employee employee = new Employee();
+        employee.nome = "FuncionarioGoku123";
+        employee.idade = 23;
+        employee.salario = new double[]{1500, 1700, 2000};
+
+        employee.imprimirDados();
+        employee.calcularMediaSalarios();
+    }
+}
+*/
