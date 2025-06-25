a665955fd4296d528e8fe7513905d892dabc25ee
GabrielNat1 <dcbiel846@gmail.com>
2025-06-25 19:13:45 -0300

    Orientação Objetos - Bloco de inicialização estático

---
+++ src/academy/devdojo/maratona/javacore/Fmodificadorestatico/test/AnimeTest01.java
@@ -0,0 +1,11 @@
+package academy.devdojo.maratona.javacore.Fmodificadorestatico.test;
+
+import academy.devdojo.maratona.javacore.Fmodificadorestatico.domain.Anime;
+
+public class AnimeTest01 {
+    public static void main(String[] args) {
+        Anime anime01 = new Anime();
+        Anime anime02 = new Anime();
+        Anime anime03 = new Anime();
+    }
+}
