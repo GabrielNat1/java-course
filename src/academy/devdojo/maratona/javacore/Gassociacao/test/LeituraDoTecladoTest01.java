03885e4544d320477f3ec2a11f49c80a4f5a7fb6
GabrielNat1 <dcbiel846@gmail.com>
2025-07-06 12:55:26 -0300

    Orientação Objetos - Leitura de dados pelo console

---
+++ src/academy/devdojo/maratona/javacore/Gassociacao/test/LeituraDoTecladoTest01.java
@@ -0,0 +1,11 @@
+package academy.devdojo.maratona.javacore.Gassociacao.test;
+
+import java.util.Scanner;
+
+public class LeituraDoTecladoTest01 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String next = in.next();
+        System.out.println(next);
+    }
+}
