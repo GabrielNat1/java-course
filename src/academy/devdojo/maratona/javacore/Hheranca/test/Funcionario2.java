25d59c45ab6ad53070d935981be822d60ecab3cb
GabrielNat1 <dcbiel846@gmail.com>
2025-07-13 19:17:42 -0300

    herança protected

---
+++ src/academy/devdojo/maratona/javacore/Hheranca/test/Funcionario2.java
@@ -0,0 +1,9 @@
+package academy.devdojo.maratona.javacore.Hheranca.test;
+
+import academy.devdojo.maratona.javacore.Hheranca.dominio.Pessoa;
+
+public class Funcionario2 extends Pessoa {
+    public void teste(){
+        this.nome = "nome";
+    }
+}
