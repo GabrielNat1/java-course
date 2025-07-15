18775f772128b83aa7bb05ff0bffba143ec0d446
GabrielNat1 <dcbiel846@gmail.com>
2025-07-15 13:31:08 -0300

    Orientação Objetos - Sobrescrita do método toString

---
+++ src/academy/devdojo/maratona/javacore/Isobrescrita/test/AnimeTest01.java
@@ -0,0 +1,8 @@
+import academy.devdojo.maratona.javacore.Isobrescrita.dominio.Anime;
+
+public class AnimeTest01 {
+    public static void main(String[] args) {
+        Anime anime = new Anime("Dragon Ball Z");
+        System.out.println(anime.getNome());
+    }
+}
