1b48178298278c92fc6b1fd13a9a11d6f09647d2
GabrielNat1 <dcbiel846@gmail.com>
2025-07-16 14:50:26 -0300

    Orientação Objetos - Enumeração pt 04 - Busca por atributos

--- src/academy/devdojo/maratona/javacore/Kenum/domain/TypeClient.java
+++ src/academy/devdojo/maratona/javacore/Kenum/domain/TypeClient.java
@@ -1,8 +1,8 @@
 package academy.devdojo.maratona.javacore.Kenum.domain;
 
 public enum TypeClient {
-    NATURAL_PERSON(1, "NATURAL_PERSON"),
-    LEGAL_ENTITY(2, "LEGAL_ENTITY");
+    NATURAL_PERSON(1, "natural person"),
+    LEGAL_ENTITY(2, "legal entity");
 
     private final int value;
     private final String relatoryName;
@@ -10,6 +10,16 @@ public enum TypeClient {
     TypeClient(int value, String relatoryName) {
         this.value = value;
         this.relatoryName = relatoryName;
+    }
+
+    public static TypeClient typeClientByRelatory(String relatoryName){
+        for (TypeClient typeClient : values()){
+            if (typeClient.getRelatoryName().equalsIgnoreCase(relatoryName)) {
+                return typeClient;
+            }
+        }
+
+        return null;
     }
 
     public int getValue() {
--- src/academy/devdojo/maratona/javacore/Kenum/test/ClientTest01.java
+++ src/academy/devdojo/maratona/javacore/Kenum/test/ClientTest01.java
@@ -6,12 +6,18 @@ import academy.devdojo.maratona.javacore.Kenum.domain.TypePayment;
 
 public class ClientTest01 {
     public static void main(String[] args) {
-        Client client1 = new Client("Goku", TypeClient.LEGAL_ENTITY, TypePayment.DEBIT_CARD);
-        Client client2 = new Client("Vegeta", TypeClient.NATURAL_PERSON, TypePayment.CREDIT_CARD);
+        Client client1 = new Client("Goku", TypeClient.NATURAL_PERSON, TypePayment.DEBIT_CARD);
+        Client client2 = new Client("Vegeta", TypeClient.LEGAL_ENTITY, TypePayment.CREDIT_CARD);
 
         System.out.println(client1);
         System.out.println(client2);
 
         System.out.println(TypePayment.DEBIT_CARD.calculateDiscount(100));
+
+        TypeClient typeClient = TypeClient.valueOf("NATURAL_PERSON");
+        System.out.println(typeClient);
+
+        TypeClient typeClient02 = TypeClient.typeClientByRelatory("legal entity");
+        System.out.println(typeClient02);
     }
 }
