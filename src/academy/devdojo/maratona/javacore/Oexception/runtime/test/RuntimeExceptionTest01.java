e911beb757c42c3b08df7e7f55676e18c6c98543
GabrielNat1 <dcbiel846@gmail.com>
2025-07-20 18:44:30 -0300

    Exceções pt 03 - Exception

--- src/academy/devdojo/maratona/javacore/Oexception/runtime/test/RuntimeExceptionTest01.java
+++ src/academy/devdojo/maratona/javacore/Oexception/runtime/test/RuntimeExceptionTest01.java
@@ -3,6 +3,7 @@ package academy.devdojo.maratona.javacore.Oexception.runtime.test;
 public class RuntimeExceptionTest01 {
     public static void main(String[] args) {
         // checked & unchecked exceptions
-        Object object = null;
+        int[] nums = {1, 2, 3};
+        System.out.println(nums[3]);
     }
 }
