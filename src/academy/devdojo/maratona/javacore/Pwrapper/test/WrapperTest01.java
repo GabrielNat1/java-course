faa0c99b6ed4907567ef27766753b025788ebdcd
GabrielNat1 <dcbiel846@gmail.com>
2025-07-22 13:54:33 -0300

    Classes Utilitárias - Wrappers pt 01

---
+++ src/academy/devdojo/maratona/javacore/Pwrapper/test/WrapperTest01.java
@@ -0,0 +1,24 @@
+package academy.devdojo.maratona.javacore.Pwrapper.test;
+
+public class WrapperTest01 {
+    public static void main(String[] args) {
+        byte byteP = 1;
+        short shortP = 1;
+        int intP = 1;
+        long longP = 10L;
+        float floatP = 10F;
+        double doubleP = 10D;
+        char charP = 'A';
+        boolean booleanP = true;
+
+        // Wrappers
+        Byte byteW = 1;
+        Short shortW = 1;
+        Integer intW = 1;
+        Long longW = 10L;
+        Float floatW = 10F;
+        Double doubleW = 10D;
+        Character charW = 'A';
+        Boolean booleanW = true;
+    }
+}
