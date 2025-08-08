b8422a9afa7ac5713931ca01380307c30d074ce5
GabrielNat1 <dcbiel846@gmail.com>
2025-08-08 17:10:51 -0300

    115 - Classes Utilitárias - Internacionalização Datas com Locale

---
+++ src/academy/devdojo/maratona/javacore/Rdates/test/LocateTest01.java
@@ -0,0 +1,19 @@
+package academy.devdojo.maratona.javacore.Rdates.test;
+
+import java.text.DateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
+public class LocateTest01 {
+    public static void main(String[] args) {
+        // pt - BR
+        Locale localeItaly = new Locale("it", "IT");
+        Locale localeBrazil = new Locale("pt", "BR");
+        Calendar calendar = Calendar.getInstance();
+        DateFormat df1 = DateFormat.getDateInstance(DateFormat.FULL, localeBrazil);
+        DateFormat df2 = DateFormat.getDateInstance(DateFormat.FULL, localeItaly);
+
+        System.out.println("Brazil: " + df1.format(calendar.getTime()));
+        System.out.println("Italia: " + df2.format(calendar.getTime()));
+    }
+}
