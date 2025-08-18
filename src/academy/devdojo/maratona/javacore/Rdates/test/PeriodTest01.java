296d498623f26129f3e8c9b5ecc82fd065f150c5
GabrielNat1 <dcbiel846@gmail.com>
2025-08-18 15:15:41 -0300

    124 - Classes Utilitárias - Period

---
+++ src/academy/devdojo/maratona/javacore/Rdates/test/PeriodTest01.java
@@ -0,0 +1,17 @@
+package academy.devdojo.maratona.javacore.Rdates.test;
+
+import java.time.LocalDate;
+import java.time.Period;
+
+public class PeriodTest01 {
+    public static void main(String[] args) {
+        LocalDate now = LocalDate.now();
+        LocalDate nowAfter10Days = now.plusDays(10);
+        Period p1 = Period.between(now, nowAfter10Days);
+        Period p2 = Period.ofDays(10);
+        Period p3 = Period.ofWeeks(1);
+        System.out.println("Period between now and 10 days later: " + p1);
+        System.out.println("Period of 10 days: " + p2);
+        System.out.println("Period of 1 week: " + p3);
+    }
+}
