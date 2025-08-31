312f6edd3a17ca5a526aa720eb161e48b1a73c96
GabrielNat1 <dcbiel846@gmail.com>
2025-08-31 19:30:44 -0300

    135 - Classes Utilitárias - Regex pt 05 - Pattern e Matcher - Quantificadores pt 02

--- src/academy/devdojo/maratona/javacore/Uregex/test/PatternMatcherTest05.java
+++ src/academy/devdojo/maratona/javacore/Uregex/test/PatternMatcherTest05.java
@@ -1,9 +1,10 @@
 package academy.devdojo.maratona.javacore.Uregex.test;
 
+import java.sql.SQLOutput;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class PatternMatcherTest03 {
+public class PatternMatcherTest05 {
     public static void main(String[] args) {
         // \d = All digits
         // \D = Everything that is not a digit
@@ -21,8 +22,10 @@ public class PatternMatcherTest03 {
         // $ End of line
         // . Any character
 
-        String regex = "0[xX]([0-9a-fA-F])+(\\s|$)";
-        String text = "12 0x 0X 0xFFABC 0x10G 0x1";
+        String regex = "([a-zA-Z0-9\\._-])+@([a-zA-Z])+(\\.([a-zA-Z])+)+";
+        String text = "luff@hotmail.com, 123jotaro@gmail.com, #@!zoro@gmail.br, teste@gmail.com, sakura@gmail.com";
+        System.out.println(" " + "#@!zoro@gmail.br".matches(regex));
+        System.out.println(text.split(", ")[1].trim());
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(text);
         System.out.println("text: " +matcher);
