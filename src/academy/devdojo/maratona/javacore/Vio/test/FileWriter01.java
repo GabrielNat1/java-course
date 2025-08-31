fe3b3c7baa61c5c101e1f2bab09cb9e19a971477
GabrielNat1 <dcbiel846@gmail.com>
2025-08-31 20:03:38 -0300

    139 - Classes Utilitárias - IO pt 02 - FileWriter

---
+++ src/academy/devdojo/maratona/javacore/Vio/test/FileWriter01.java
@@ -0,0 +1,23 @@
+package academy.devdojo.maratona.javacore.Vio.test;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+// File
+// FileWriter
+// FileReader
+// BufferedReader
+// BufferedWriter
+public class FileWriter01 {
+    public static void main(String[] args) {
+        File file = new File("file.txt");
+        try(FileWriter fw = new FileWriter(file, true)) {
+            fw.write("teste");
+            fw.flush();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+}
