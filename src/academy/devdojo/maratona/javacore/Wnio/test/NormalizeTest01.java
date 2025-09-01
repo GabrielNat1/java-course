d3e6b9422e3ea45efd2fc24899ee20623aee9947
GabrielNat1 <dcbiel846@gmail.com>
2025-09-01 15:24:07 -0300

    146 - Classes Utilitárias - NIO pt 03 - Normalização

---
+++ src/academy/devdojo/maratona/javacore/Wnio/test/NormalizeTest01.java
@@ -0,0 +1,17 @@
+package academy.devdojo.maratona.javacore.Wnio.test;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class NormalizeTest01 {
+    public static void main(String[] args) {
+        String pathProject = "home/gabriel/dev";
+        Path path1 = Paths.get(pathProject, "file.txt");
+        System.out.println(path1);
+        System.out.println(path1.normalize());
+
+        Path path2 = Paths.get("home/./gabriel/./dev/./file.txt");
+        System.out.println(path2);
+        System.out.println(path2.normalize());
+    }
+}
