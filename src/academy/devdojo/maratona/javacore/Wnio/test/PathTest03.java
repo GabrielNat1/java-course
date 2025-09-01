73445e9f5958ea5945f55134ec26acc909a660dc
GabrielNat1 <dcbiel846@gmail.com>
2025-09-01 15:39:38 -0300

    fix: 147 - Classes Utilitárias - NIO pt 04 - Resolvendo Paths

--- src/academy/devdojo/maratona/javacore/Wnio/test/PathTest03.java
+++ src/academy/devdojo/maratona/javacore/Wnio/test/PathTest03.java
@@ -4,19 +4,19 @@ import java.nio.file.Path;
 
 public class PathTest03 {
     public static void main(String[] args) {
-        Path dir = Path.get("home/gabriel");
-        Path file = Path.get("dev/file.txt");
-        Path resolve = dir.resolve(file);
+        Path dir = Path.of("home/gabriel");
+        Path file1 = Path.of("dev/file.txt");
+        Path resolve = dir.resolve(file1);
         System.out.println(resolve);
 
-        Path absolute = Path.get("/home/gabriel");
-        Path relative = Path.get("dev");
-        Path file = Path.get("file.txt");
+        Path absolute = Path.of("/home/gabriel");
+        Path relative = Path.of("dev");
+        Path file2 = Path.of("file.txt");
         System.out.println("1 "+absolute.resolve(relative));
-        System.out.println("2 "+absolute.resolve(file));
+        System.out.println("2 "+absolute.resolve(file2));
         System.out.println("3 "+relative.resolve(absolute));
-        System.out.println("4 "+relative.resolve(file));
-        System.out.println("5 "+file.resolve(absolute));
-        System.out.println("6 "+file.resolve(relative));
+        System.out.println("4 "+relative.resolve(file2));
+        System.out.println("5 "+file2.resolve(absolute));
+        System.out.println("6 "+file2.resolve(relative));
     }
 }
