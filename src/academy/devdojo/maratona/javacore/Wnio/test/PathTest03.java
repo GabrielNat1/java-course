12d97fdc47c8129e71c86ddff36528ed4121f774
GabrielNat1 <dcbiel846@gmail.com>
2025-09-01 15:34:04 -0300

    147 - Classes Utilitárias - NIO pt 04 - Resolvendo Paths

---
+++ src/academy/devdojo/maratona/javacore/Wnio/test/PathTest03.java
@@ -0,0 +1,22 @@
+package academy.devdojo.maratona.javacore.Wnio.test;
+
+import java.nio.file.Path;
+
+public class PathTest03 {
+    public static void main(String[] args) {
+        Path dir = Path.get("home/gabriel");
+        Path file = Path.get("dev/file.txt");
+        Path resolve = dir.resolve(file);
+        System.out.println(resolve);
+
+        Path absolute = Path.get("/home/gabriel");
+        Path relative = Path.get("dev");
+        Path file = Path.get("file.txt");
+        System.out.println("1 "+absolute.resolve(relative));
+        System.out.println("2 "+absolute.resolve(file));
+        System.out.println("3 "+relative.resolve(absolute));
+        System.out.println("4 "+relative.resolve(file));
+        System.out.println("5 "+file.resolve(absolute));
+        System.out.println("6 "+file.resolve(relative));
+    }
+}
