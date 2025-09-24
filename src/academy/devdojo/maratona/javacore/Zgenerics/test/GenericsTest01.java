ae5fbe96c48fb9087c1f37335010d6a7e26a7708
GabrielNat1 <dcbiel846@gmail.com>
2025-09-24 10:35:38 -0300

    183 - Generics pt 01 - Introdução

---
+++ src/academy/devdojo/maratona/javacore/Zgenerics/test/GenericsTest01.java
@@ -0,0 +1,29 @@
+package academy.devdojo.maratona.javacore.Zgenerics.test;
+
+import academy.devdojo.maratona.javacore.Ycolections.domain.Consumer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GenericsTest01 {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        list.add("Midoriya");
+        list.add("Midoriya");
+
+
+        for (String o : list) {
+            System.out.println(o);
+        }
+        add(list, new Consumer("Midoriya"));
+
+        for (Object o : list) {
+            System.out.println(o);
+        }
+
+    }
+
+    private static void add(List list, Consumer consumer) {
+        list.add(consumer);
+    }
+}
