f116429bc6e709ca28b1a9a20a1b42b6db383912
GabrielNat1 <dcbiel846@gmail.com>
2025-09-28 18:05:52 -0300

    195 - Lambdas pt 01 - Predicate

--- src/academy/devdojo/maratona/javacore/ZZBbehavior/interfaces/CarPredicate.java
+++ src/academy/devdojo/maratona/javacore/ZZBbehavior/interfaces/CarPredicate.java
@@ -2,6 +2,7 @@ package academy.devdojo.maratona.javacore.ZZBbehavior.interfaces;
 
 import academy.devdojo.maratona.javacore.ZZBbehavior.domain.Car;
 
+@FunctionalInterface
 public interface CarPredicate {
     boolean test(Car car);
 }
