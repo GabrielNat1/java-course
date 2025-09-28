b6ed9afa9b3e295524a673d81ff1d46d4b3f92d7
GabrielNat1 <dcbiel846@gmail.com>
2025-09-28 17:42:23 -0300

    194 - Parametrizando comportamentos pt 02

---
+++ src/academy/devdojo/maratona/javacore/ZZBbehavior/interfaces/CarPredicate.java
@@ -0,0 +1,7 @@
+package academy.devdojo.maratona.javacore.ZZBbehavior.interfaces;
+
+import academy.devdojo.maratona.javacore.ZZBbehavior.domain.Car;
+
+public interface CarPredicate {
+    boolean test(Car car);
+}
