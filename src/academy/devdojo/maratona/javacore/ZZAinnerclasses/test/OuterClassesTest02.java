e4256276daa210c978be6f0a0803df37de5813d0
GabrielNat1 <dcbiel846@gmail.com>
2025-09-28 16:51:16 -0300

    190 - Classes Internas pt 02 - Classes Locais

---
+++ src/academy/devdojo/maratona/javacore/ZZAinnerclasses/test/OuterClassesTest02.java
@@ -0,0 +1,22 @@
+package academy.devdojo.maratona.javacore.ZZAinnerclasses.test;
+
+public class OuterClassesTest02 {
+    private String name = "name";
+
+    void print(final String param){
+        final String lastName = "lastName";
+        class LocalClass {
+            public void printLocal(){
+                System.out.println(param);
+                System.out.println(name + " " + lastName);
+            }
+        }
+        LocalClass localClass = new LocalClass();
+        localClass.printLocal();
+    }
+
+    public static void main(String[] args) {
+        OuterClassesTest02 outerClassesTest02 = new OuterClassesTest02();
+        outerClassesTest02.print();
+    }
+}
