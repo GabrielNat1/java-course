7e836659316ec173850d0d3e1bb66abaf3cf0151
GabrielNat1 <dcbiel846@gmail.com>
2025-10-18 13:38:39 -0300

    224 - Threads pt 05 - Sincronismo de thread pt 01

---
+++ src/academy/devdojo/maratona/javacore/ZZFthreads/test/ThreadAccountTest01.java
@@ -0,0 +1,41 @@
+package academy.devdojo.maratona.javacore.ZZFthreads.test;
+
+import academy.devdojo.maratona.javacore.ZZFthreads.domain.Account;
+
+public class ThreadAccountTest01 implements Runnable {
+    private Account account = new Account();
+
+    public static void main(String[] args) {
+        ThreadAccountTest01 threadAccountTest01 = new ThreadAccountTest01();
+        Thread t1 = new Thread(threadAccountTest01, "a");
+        Thread t2 = new Thread(threadAccountTest01, "b");
+
+        t1.start();
+        t2.start();
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 5; i++) {
+            withdrawal(10);
+            if (account.getBalance() < 0){
+                System.out.println("F");
+            }
+        }
+    }
+
+    private void withdrawal(int amount){
+        if (account.getBalance() >= amount){
+            System.out.println(getThreadName() + " withdrawal money");
+            account.withdrawal(amount);
+            System.out.println(getThreadName() + "value in balance: " + account.getBalance());
+        } else {
+            System.out.println("without money " + getThreadName());
+        }
+    }
+
+    private String getThreadName(){
+        return Thread.currentThread().getName();
+    }
+
+}
