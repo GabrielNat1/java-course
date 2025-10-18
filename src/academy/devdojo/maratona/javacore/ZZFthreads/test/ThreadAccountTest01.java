906602b30dbff8ff5c83ea895af1c7e9241ba288
GabrielNat1 <dcbiel846@gmail.com>
2025-10-18 14:33:28 -0300

    225 - Threads pt 06 - Sincronismo de thread pt 02

--- src/academy/devdojo/maratona/javacore/ZZFthreads/test/ThreadAccountTest01.java
+++ src/academy/devdojo/maratona/javacore/ZZFthreads/test/ThreadAccountTest01.java
@@ -3,7 +3,7 @@ package academy.devdojo.maratona.javacore.ZZFthreads.test;
 import academy.devdojo.maratona.javacore.ZZFthreads.domain.Account;
 
 public class ThreadAccountTest01 implements Runnable {
-    private Account account = new Account();
+    private final Account account = new Account();
 
     public static void main(String[] args) {
         ThreadAccountTest01 threadAccountTest01 = new ThreadAccountTest01();
@@ -25,12 +25,21 @@ public class ThreadAccountTest01 implements Runnable {
     }
 
     private void withdrawal(int amount){
-        if (account.getBalance() >= amount){
-            System.out.println(getThreadName() + " withdrawal money");
-            account.withdrawal(amount);
-            System.out.println(getThreadName() + "value in balance: " + account.getBalance());
-        } else {
-            System.out.println("without money " + getThreadName());
+        System.out.println(getThreadName() + " ############## not synchronized");
+        synchronized (account) {
+            System.out.println(getThreadName() + " ******** synchronized");
+            if (account.getBalance() >= amount){
+                System.out.println(getThreadName() + " withdrawal money");
+                account.withdrawal(amount);
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                System.out.println(getThreadName() + "value in balance: " + account.getBalance());
+            } else {
+                System.out.println("without money " + getThreadName());
+            }
         }
     }
 
