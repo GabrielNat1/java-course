18900983272c05ba723df87e85665bdaa4c76732
GabrielNat1 <dcbiel846@gmail.com>
2025-10-18 13:03:34 -0300

    222 - Threads pt 03 - Prioridade e Sleep

--- src/academy/devdojo/maratona/javacore/ZZFthreads/test/ThreadTest01.java
+++ src/academy/devdojo/maratona/javacore/ZZFthreads/test/ThreadTest01.java
@@ -35,7 +35,11 @@ class ThreadExampleRunnable implements Runnable{
             if (i % 100 == 0){
                 System.out.println();
             }
-
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
         }
     }
 }
@@ -47,10 +51,12 @@ public class ThreadTest01 {
 //        ThreadExample t3 = new ThreadExample('C');
 //        ThreadExample t4 = new ThreadExample('D');
 
-        Thread t1 = new Thread(new ThreadExampleRunnable('A'));
-        Thread t2 = new Thread(new ThreadExampleRunnable('A'));
-        Thread t3 = new Thread(new ThreadExampleRunnable('A'));
-        Thread t4 = new Thread(new ThreadExampleRunnable('A'));
+        Thread t1 = new Thread(new ThreadExampleRunnable('A'), "T1A");
+        Thread t2 = new Thread(new ThreadExampleRunnable('A'), "T2B");
+        Thread t3 = new Thread(new ThreadExampleRunnable('A'), "T3C");
+        Thread t4 = new Thread(new ThreadExampleRunnable('A'), "T4D");
+
+        t4.setPriority(Thread.MAX_PRIORITY);
 
         t1.start();
         t2.start();
